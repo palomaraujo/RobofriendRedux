4a0d23ae0a5014259cf9e0167a140ebc8876ced2
Palomaraujo <palomaraujo4@gmail.com>
2021-05-02 22:17:21 -0300

    initial commit

---
+++ src/components/CardList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Card from './Card'
+
+const CardList = ({ robots }) => {
+
+    /*
+    if(true){
+        throw new Error('NOOOOOO');
+    }
+    */
+
+    return (
+        <div>
+           {
+            robots.map((user, index) => {
+                return(
+                    <Card 
+                        key={robots[index].id}
+                        id={ robots[index].id } 
+                        name= { robots[index].name} 
+                        email= { robots[index].email }
+                    />
+                );
+            })
+           }
+        </div>
+    );
+}
+
+export default CardList;
